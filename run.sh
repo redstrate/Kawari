e26578a1fa64a1983a17a4a8151bc57f45392efe
Joshua Goins <josh@redstrate.com>
2024-05-11 12:26:09 -0400

    Add run.sh script to help running all of them at the same time

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+trap 'kill $(jobs -p)' EXIT
+
+cargo run -q --package kawari --bin kawari-admin &
+cargo run -q --package kawari --bin kawari-frontier &
+cargo run -q --package kawari --bin kawari-login &
+cargo run -q --package kawari --bin kawari-patch &
+wait
