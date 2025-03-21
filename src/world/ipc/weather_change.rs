b3a22ebdcea038b01494dcdbe8fc5b56a6eb72cf
Joshua Goins <josh@redstrate.com>
2025-03-21 19:58:48 -0400

    Fix size of WeatherChange packet

--- src/world/ipc/weather_change.rs
+++ src/world/ipc/weather_change.rs
@@ -4,6 +4,6 @@ use binrw::binrw;
 #[derive(Debug, Clone, Copy, Default)]
 pub struct WeatherChange {
     pub weather_id: u16,
-    #[brw(pad_before = 3)]
+    #[brw(pad_before = 2)]
     pub transistion_time: f32,
 }
