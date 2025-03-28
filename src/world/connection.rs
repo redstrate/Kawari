cb740a2a9300d950cc4eb46ae9be8d03e8224202
Joshua Goins <josh@redstrate.com>
2025-03-28 00:26:31 -0400

    Move LuaPlayer to it's own module

--- src/world/connection.rs
+++ src/world/connection.rs
@@ -1,4 +1,3 @@
-use mlua::{UserData, UserDataMethods};
 use tokio::net::TcpStream;
 
 use crate::{
@@ -11,7 +10,7 @@ use crate::{
 };
 
 use super::{
-    Inventory, Item, Zone,
+    Inventory, Item, LuaPlayer, Zone,
     ipc::{
         ActorSetPos, ClientZoneIpcSegment, ContainerInfo, ContainerType, InitZone, ItemInfo,
         ServerZoneIpcData, ServerZoneIpcSegment, StatusEffect, StatusEffectList, UpdateClassInfo,
@@ -323,56 +322,5 @@ impl ZoneConnection {
 
             self.status_effects.dirty = false;
         }
-    }
-}
-
-#[derive(Default)]
-pub struct LuaPlayer {
-    pub player_data: PlayerData,
-    pub status_effects: StatusEffects,
-    queued_segments: Vec<PacketSegment<ServerZoneIpcSegment>>,
-}
-
-impl LuaPlayer {
-    fn queue_segment(&mut self, segment: PacketSegment<ServerZoneIpcSegment>) {
-        self.queued_segments.push(segment);
-    }
-
-    fn send_message(&mut self, message: &str) {
-        let ipc = ServerZoneIpcSegment {
-            op_code: ServerZoneIpcType::ServerChatMessage,
-            timestamp: timestamp_secs(),
-            data: ServerZoneIpcData::ServerChatMessage {
-                message: message.to_string(),
-                unk: 0,
-            },
-            ..Default::default()
-        };
-
-        self.queue_segment(PacketSegment {
-            source_actor: self.player_data.actor_id,
-            target_actor: self.player_data.actor_id,
-            segment_type: SegmentType::Ipc { data: ipc },
-        });
-    }
-
-    fn give_status_effect(&mut self, effect_id: u16, duration: f32) {
-        self.status_effects.add(effect_id, duration);
-    }
-}
-
-impl UserData for LuaPlayer {
-    fn add_methods<M: UserDataMethods<Self>>(methods: &mut M) {
-        methods.add_method_mut("send_message", |_, this, message: String| {
-            this.send_message(&message);
-            Ok(())
-        });
-        methods.add_method_mut(
-            "give_status_effect",
-            |_, this, (effect_id, duration): (u16, f32)| {
-                this.give_status_effect(effect_id, duration);
-                Ok(())
-            },
-        );
     }
 }
