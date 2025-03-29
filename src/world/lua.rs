c4b9ad060b4675811dcc68be8c59110089f8e110
Joshua Goins <josh@redstrate.com>
2025-03-28 21:28:30 -0400

    Add basic Event scripting capaibilities
    This implements the first event script, going from the first Ul'dah opening
    cutscene to the next.

--- src/world/lua.rs
+++ src/world/lua.rs
@@ -1,14 +1,14 @@
 use mlua::{UserData, UserDataMethods};
 
 use crate::{
-    common::timestamp_secs,
+    common::{ObjectId, ObjectTypeId, timestamp_secs},
     opcodes::ServerZoneIpcType,
     packet::{PacketSegment, SegmentType},
 };
 
 use super::{
     PlayerData, StatusEffects,
-    ipc::{ServerZoneIpcData, ServerZoneIpcSegment},
+    ipc::{EventPlay, ServerZoneIpcData, ServerZoneIpcSegment},
 };
 
 #[derive(Default)]
@@ -44,6 +44,33 @@ impl LuaPlayer {
     fn give_status_effect(&mut self, effect_id: u16, duration: f32) {
         self.status_effects.add(effect_id, duration);
     }
+
+    fn play_scene(&mut self, event_id: u32, scene: u16, scene_flags: u32, param: u8) {
+        let ipc = ServerZoneIpcSegment {
+            unk1: 20,
+            unk2: 0,
+            op_code: ServerZoneIpcType::EventPlay,
+            server_id: 0,
+            timestamp: timestamp_secs(),
+            data: ServerZoneIpcData::EventPlay(EventPlay {
+                actor_id: ObjectTypeId {
+                    object_id: ObjectId(self.player_data.actor_id),
+                    object_type: 0,
+                },
+                event_id,
+                scene,
+                scene_flags,
+                unk2: param,
+                ..Default::default()
+            }),
+        };
+
+        self.queue_segment(PacketSegment {
+            source_actor: self.player_data.actor_id,
+            target_actor: self.player_data.actor_id,
+            segment_type: SegmentType::Ipc { data: ipc },
+        });
+    }
 }
 
 impl UserData for LuaPlayer {
@@ -56,6 +83,13 @@ impl UserData for LuaPlayer {
             "give_status_effect",
             |_, this, (effect_id, duration): (u16, f32)| {
                 this.give_status_effect(effect_id, duration);
+                Ok(())
+            },
+        );
+        methods.add_method_mut(
+            "play_scene",
+            |_, this, (event_id, scene, scene_flags, param): (u32, u16, u32, u8)| {
+                this.play_scene(event_id, scene, scene_flags, param);
                 Ok(())
             },
         );
