6bb6377315f05547b001703c2c166338f1f9b25c
Joshua Goins <josh@redstrate.com>
2025-03-28 22:52:21 -0400

    Add stubs for the other two starting City-States
    Also document how to access and play these scenes in USAGE.

---
+++ resources/scripts/opening/OpeningLimsaLominsa.lua
@@ -0,0 +1,27 @@
+--- TODO: find a way to hardcode it this way
+EVENT_ID = 1245185
+
+--- load defines from Opening Excel sheet, which has this and we don't need to hardcode it'
+POS_START = 4101800
+
+function Scene00000(player)
+    player:play_scene(EVENT_ID, 00000, 4959237, 1)
+end
+
+function Scene00001(player)
+    player:play_scene(EVENT_ID, 00001, 4959237, 1)
+end
+
+function onEnterTerritory(player, zone)
+    --- move the player into the starting position
+    start_pos = zone:get_pop_range(POS_START)
+    player:set_position(start_pos)
+
+    Scene00000(player);
+end
+
+function onSceneFinished(player, scene)
+    if scene == 0 then
+    Scene00001(player)
+    end
+end
