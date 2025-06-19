d6f98cd2414834bce1a9af51ebf7e9165e8526e0
The Dax <thedax@users.noreply.github.com>
2025-06-19 11:25:40 -0400

    Minor Festival.lua cleanup

--- resources/scripts/commands/debug/Festival.lua
+++ resources/scripts/commands/debug/Festival.lua
@@ -6,15 +6,16 @@ function onCommand(args, player)
     local argc = table.getn(parts)
     local usage = "\nUsage: !festival <id> <arg1> <arg2> <arg3>"
     local sender = "[festival] "
+
     if argc < 4 then
         player:send_message(sender.."This command requires 4 parameters."..usage)
         return
     end
 
-    arg1 = tonumber(parts[1])
-    arg2 = tonumber(parts[2])
-    arg3 = tonumber(parts[3])
-    arg4 = tonumber(parts[4])
+    local arg1 = tonumber(parts[1])
+    local arg2 = tonumber(parts[2])
+    local arg3 = tonumber(parts[3])
+    local arg4 = tonumber(parts[4])
     
     if not arg1 or not arg2 or not arg3 or not arg4 then
         player:send_message(sender.."Error parsing parameters. Make sure your inputs are integers."..usage)
