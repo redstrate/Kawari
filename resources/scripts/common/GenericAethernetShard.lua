2abb44b6d9d8f99bd5f65988599ff3218116666d
The Dax <thedax@users.noreply.github.com>
2025-06-22 16:06:42 -0400

    Bring up the aethernet -Implemented all aethernet shards -Implemented more lift/ferry skipper NPCs that don't require special handling -Commented the mysterious GenericWarps at the beginning of the list

---
+++ resources/scripts/common/GenericAethernetShard.lua
@@ -0,0 +1,18 @@
+-- generic aetheryte, use this for all of the mini aetherytes
+
+--- scene 02 - aetheryte menu
+--- scene 03 - "you have aethernet access" message and vfx
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 00002, 8192, 0)
+end
+
+function onReturn(scene, results, player)
+    local AETHERNET_MENU_CANCEL = 0
+    local destination = tonumber(results[1])
+    player:finish_event(EVENT_ID)
+    
+    if destination ~= AETHERNET_MENU_CANCEL then
+        player:warp_aetheryte(destination)
+    end
+end
