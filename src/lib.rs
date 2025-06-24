a1c82448614fd51cf5e526173753b1869ab662f1
The Dax <thedax@users.noreply.github.com>
2025-06-24 15:51:57 -0400

    Attempt to update opcodes for 7.25h2, but it's possible I missed some

--- src/lib.rs
+++ src/lib.rs
@@ -48,14 +48,14 @@ pub const RECEIVE_BUFFER_SIZE: usize = 32000;
 pub const SUPPORTED_BOOT_VERSION: Version = Version("2025.05.01.0000.0001");
 
 /// Supported game version.
-pub const SUPPORTED_GAME_VERSION: Version = Version("2025.06.10.0000.0000");
+pub const SUPPORTED_GAME_VERSION: Version = Version("2025.06.19.0000.0000");
 
 const SUPPORTED_EXPAC_VERSIONS: [(&str, Version); 5] = [
     ("ex1", Version("2025.05.01.0000.0000")),
     ("ex2", Version("2025.05.09.0000.0000")),
-    ("ex3", Version("2025.06.10.0000.0000")),
-    ("ex4", Version("2025.06.10.0000.0000")),
-    ("ex5", Version("2025.06.10.0000.0000")),
+    ("ex3", Version("2025.06.19.0000.0000")),
+    ("ex4", Version("2025.06.19.0000.0000")),
+    ("ex5", Version("2025.06.19.0000.0000")),
 ];
 
 /// Supported expansion versions.
