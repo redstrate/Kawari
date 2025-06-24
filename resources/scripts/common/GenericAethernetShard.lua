feb0ffb8695108e8f5e1399f5fbc8d3a31235197
The Dax <thedax@users.noreply.github.com>
2025-06-24 18:23:01 -0400

    Implement the aethernet for aetherytes and ensure both aetherytes and aethernet shards do better about not yeeting you into invalid zones, crashing the client, or softlocking

--- resources/scripts/common/GenericAethernetShard.lua
+++ resources/scripts/common/GenericAethernetShard.lua
@@ -1,18 +1,32 @@
 -- generic aetheryte, use this for all of the aethernet shards
 
---- scene 02 - aetheryte menu
---- scene 03 - "you have aethernet access" message and vfx
+--- scene 00000 - does nothing
+--- scene 00001 - does nothing
+--- scene 00002 - aetheryte menu
+--- scene 00003 - "you have aethernet access" message and vfx
+--- scene 00100 - "According to the message engraved in the base, special permission is required to use this aetheryte." (Eulmore-specific)
+--- scene 00200 - "The aetheryte has ceased functioning." (Eulmore-specific)
+
+SCENE_SHOW_MENU = 00002
+SCENE_HAVE_AETHERNET_ACCESS = 00003
 
 function onTalk(target, player)
-    player:play_scene(target, EVENT_ID, 00002, 8192, 0)
+    player:play_scene(target, EVENT_ID, SCENE_SHOW_MENU, 8192, 0)
 end
 
 function onReturn(scene, results, player)
     local AETHERNET_MENU_CANCEL = 0
     local destination = results[1]
-    player:finish_event(EVENT_ID)
-    
-    if destination ~= AETHERNET_MENU_CANCEL then
-        player:warp_aetheryte(destination)
+
+    if scene == SCENE_SHOW_MENU then
+        if destination ~= AETHERNET_MENU_CANCEL then
+            player:finish_event(EVENT_ID) -- Need to finish the event here, because warping does not return to this callback (the game will crash or softlock otherwise)
+            player:warp_aetheryte(destination)
+            return
+        end
+    --elseif scene == HAVE_AETHERNET_ACCESS then
+        -- TODO: attunement logic
     end
+
+    player:finish_event(EVENT_ID)
 end
