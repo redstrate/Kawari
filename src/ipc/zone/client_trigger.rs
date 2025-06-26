7d203d1de537b9c7b6381b7199d9c0bcb60205bb
Joshua Goins <josh@redstrate.com>
2025-06-25 20:58:21 -0400

    Add documentation about the supported client triggers

--- src/ipc/zone/client_trigger.rs
+++ src/ipc/zone/client_trigger.rs
@@ -5,6 +5,7 @@ use crate::common::{read_bool_from, write_bool_as};
 #[binrw]
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub enum ClientTriggerCommand {
+    /// The player sheathes/unsheathes their weapon.
     #[brw(magic = 0x1u16)]
     ToggleWeapon {
         #[brw(pad_before = 2)]
@@ -12,6 +13,7 @@ pub enum ClientTriggerCommand {
         #[bw(map = write_bool_as::<u32>)]
         shown: bool,
     },
+    /// The player looks or stops looking at an actor.
     #[brw(magic = 0x3u16)]
     SetTarget {
         #[brw(pad_before = 2)]
@@ -21,23 +23,27 @@ pub enum ClientTriggerCommand {
     Unk1 {},
     #[brw(magic = 0xC9u16)]
     Unk2 {},
+    /// The player begins an emote.
     #[brw(magic = 0x1F4u16)]
     Emote {
         #[brw(pad_before = 2)] // padding
         emote: u32,
     },
+    /// The player explicitly changed their pose.
     #[brw(magic = 0x1F9u16)]
     ChangePose {
         #[brw(pad_before = 2)] // padding
         unk1: u32,
         pose: u32,
     },
+    /// The client is "reapplying" the existing pose, like after idling.
     #[brw(magic = 0x1FAu16)]
     ReapplyPose {
         #[brw(pad_before = 2)] // padding
         unk1: u32,
         pose: u32,
     },
+    /// The player selects a teleport destination.
     #[brw(magic = 0xCAu16)]
     TeleportQuery {
         #[brw(pad_before = 2)]
