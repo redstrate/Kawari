f056b3f97894ad23e99a9d9194651080bd0bf02b
Joshua Goins <josh@redstrate.com>
2025-07-04 16:29:42 -0400

    Finish implementing the Unending Journey (mostly)
    The cutscenes actually play now, which is super swell! There's a
    few missing bits and bobs, but we can consider this event mostly
    finished now.

--- resources/scripts/events/tosort/UnendingJourney.lua
+++ resources/scripts/events/tosort/UnendingJourney.lua
@@ -1,10 +1,17 @@
--- TODO: actually implement this menu
+SCENE_SHOW_MENU       = 00000
+SCENE_PLAY_CUTSCENE   = 00001
 
 function onTalk(target, player)
     -- you cannot consult, which is good because we don't know how to implement this anyway
-    player:play_scene(target, EVENT_ID, 00000, 8192, {0})
+    player:play_scene(target, EVENT_ID, SCENE_SHOW_MENU, 8192, {0})
 end
 
 function onReturn(scene, results, player)
+    if scene == 0 then
+        -- TODO: this is not the correct cutscene flags
+        -- TODO: we need to switch the player into viewingcutscene online status
+        player:play_scene(player.id, EVENT_ID, SCENE_PLAY_CUTSCENE, 8192, results)
+        return
+    end
     player:finish_event(EVENT_ID)
 end
