a1270ec012b196abf4212391f0bb577b147a776e
The Dax <thedax@users.noreply.github.com>
2025-07-05 16:04:48 -0400

    Return control to the menu after the cutscene finishes

--- resources/scripts/events/tosort/UnendingJourney.lua
+++ resources/scripts/events/tosort/UnendingJourney.lua
@@ -5,17 +5,21 @@ SCENE_SHOW_MENU       = 00000
 SCENE_PLAY_CUTSCENE   = 00001
 
 function onTalk(target, player)
-    -- you cannot consult, which is good because we don't know how to implement this anyway
     player:play_scene(target, EVENT_ID, SCENE_SHOW_MENU, 8192, {0})
 end
 
 function onReturn(scene, results, player)
-    -- A result of zero means the user exited the menu without playing anything.
-    if scene == 0 and results[1] ~= 0 then
+    local NO_SCENE <const> = 0
+    local decision <const> = results[1]
+
+    if scene == SCENE_SHOW_MENU and decision ~= NO_SCENE then
         -- TODO: we need to switch the player into viewingcutscene online status (on the Rust side?)
         player:play_scene(player.id, EVENT_ID, SCENE_PLAY_CUTSCENE, SET_BASE, results)
-        return
-        -- TODO: we cannot nest cutscenes right now, but control should return back to the UEJ menu when a cutscene finishes
+    elseif scene == SCENE_PLAY_CUTSCENE then
+        --[[ TODO: How can we make it fade back in smoothly after the cutscene finishes?
+            Could it be related to ActorControl(ViewingCutscene)? ]]
+        player:play_scene(player.id, EVENT_ID, SCENE_SHOW_MENU, 8192, {1})
+    else
+        player:finish_event(EVENT_ID)
     end
-    player:finish_event(EVENT_ID)
 end
