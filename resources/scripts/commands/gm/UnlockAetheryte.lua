9d106c705095f0d9e8d6784e409968ce04682f4e
thedax <thedax@users.noreply.github.com>
2025-07-20 06:52:01 -0400

    Fix GM commands aetheryte and item (#128)
    * Fix //gm item and //gm aetheryte
    -Aetheryte's command opcode changed some time in the last few weeks apparently
    -Item wasn't working, now it does, and additionally supports a quantity value now
    -Aetheryte's on value is properly inverted from the Lua side

--- resources/scripts/commands/gm/UnlockAetheryte.lua
+++ resources/scripts/commands/gm/UnlockAetheryte.lua
@@ -2,8 +2,8 @@ required_rank = GM_RANK_DEBUG
 command_sender = "[unlockaetheryte] "
 
 function onCommand(args, player)
-    local on = args[1] -- TODO: reverse
-    local id = args[2]
+    local on <const> = ~args[1] & 1  -- The client sends 1 for off and 0 for on, so we need to invert this for the rust side to work properly.
+    local id <const> = args[2]
 
     player:unlock_aetheryte(on, id)
     printf(player, "Aetheryte(s) %s had their unlocked status changed!", id)
