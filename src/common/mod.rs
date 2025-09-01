4b90bf03a2d515d28050215499ba2259bc6fdf9b
The Dax <thedax@users.noreply.github.com>
2025-09-01 03:59:52 -0400

    Address "warning: variables can be used directly in the `format!` string"

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -47,7 +47,7 @@ impl std::fmt::Display for ObjectId {
 
 impl std::fmt::Debug for ObjectId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "ObjectId ({})", self)
+        write!(f, "ObjectId ({self})")
     }
 }
 
