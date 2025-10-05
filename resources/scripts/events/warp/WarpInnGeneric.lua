f8191b4e8786bb90e18c818b6f7cf8a1c696412c
Joshua Goins <josh@redstrate.com>
2025-10-05 11:25:24 -0400

    Various fixes for Carline Canopy NPCs
    And more importantly, the Inn NPCs now greet you before showing the
    inn menu!

--- resources/scripts/events/warp/WarpInnGeneric.lua
+++ resources/scripts/events/warp/WarpInnGeneric.lua
@@ -1,16 +1,21 @@
 function onTalk(target, player)
-    -- has inn access
-    player:play_scene(target, EVENT_ID, 00001, 8192, {0})
+    -- greeting
+    player:play_scene(target, EVENT_ID, 00000, 8192, {0})
 
     -- doesn't have inn access
     --player:play_scene(actorId, EVENT_ID, 00002, 8192, 0)
 end
 
 function onReturn(scene, results, player)
-    player:finish_event(EVENT_ID, 0)
+    if scene == 0 then
+        -- has inn access
+        player:play_scene(player.id, EVENT_ID, 00001, 8192, {0})
+    else
+        player:finish_event(EVENT_ID, 0)
 
-    if results[1] == 1 then
-        -- get warp
-        player:warp(EVENT_ID)
+        if results[1] == 1 then
+            -- get warp
+            player:warp(EVENT_ID)
+        end
     end
 end
