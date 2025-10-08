ed4fc24543ab2df7f501f7d29ed6d745e1837667
Joshua Goins <josh@redstrate.com>
2025-10-08 18:06:17 -0400

    Make the togglemount commad more resilient
    Someone tried to type !togglemount all and it crashed their game.
    Even though I wasn't able to reproduce it, it should have a bit
    better error handling.

--- resources/scripts/commands/debug/ToggleMount.lua
+++ resources/scripts/commands/debug/ToggleMount.lua
@@ -2,6 +2,16 @@ required_rank = GM_RANK_DEBUG
 command_sender = "[togglemount] "
 
 function onCommand(args, player)
+    local argc = #args
+    if argc ~= 1 then
+        printf(player, "Incorrect arguments given!")
+        return
+    end
+
     local id = args[1]
-    player:toggle_mount(id)
+    if tonumber(id) then
+        player:toggle_mount(id)
+    else
+        printf(player, "Incorrect arguments given!")
+    end
 end
