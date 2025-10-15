0c09e008f47c9d8de87d5403533c0a3ec14e860b
Joshua Goins <josh@redstrate.com>
2025-10-15 17:18:08 -0400

    Show menu for the greeter adventurers guild NPC

--- resources/scripts/events/custom/000/RegFstAdvGuild_00005.lua
+++ resources/scripts/events/custom/000/RegFstAdvGuild_00005.lua
@@ -1,7 +1,14 @@
+-- Scene 0: Default greeting
+-- Scene 1: Regular menu asking stuff like "What do you do here?"
+
 function onTalk(target, player)
-    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {0})
+    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {})
 end
 
 function onReturn(scene, results, player)
-    player:finish_event(EVENT_ID, 0)
+    if scene == 0 then
+        player:play_scene(player.id, EVENT_ID, 00001, HIDE_HOTBAR, {})
+    else
+        player:finish_event(EVENT_ID, 0)
+    end
 end
