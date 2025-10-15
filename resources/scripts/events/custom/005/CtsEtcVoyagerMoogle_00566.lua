fe3fb3eafccde401637b9e1c27e0d960900271f7
Joshua Goins <josh@redstrate.com>
2025-10-15 17:38:49 -0400

    Add initial support for Itinerant Moogles

---
+++ resources/scripts/events/custom/005/CtsEtcVoyagerMoogle_00566.lua
@@ -0,0 +1,13 @@
+-- TODO: open the shop menus when requested. this is a specialshop, but requires event nesting
+
+-- scene 0: menu
+-- scene 1: obtain the mogpendium
+-- scene 2: open the mogpendium only
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 0, 0, {0})
+end
+
+function onReturn(scene, results, player)
+    player:finish_event(EVENT_ID, 0)
+end
