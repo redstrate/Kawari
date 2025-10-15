e1008ff12545e4ce05bdd86e5f8383d900225343
Joshua Goins <josh@redstrate.com>
2025-10-15 17:20:54 -0400

    Add semi-working event script for frontline attendants

---
+++ resources/scripts/events/custom/001/ComDefFrontLine_00182.lua
@@ -0,0 +1,11 @@
+-- scene 0: initial greeting, and then nothing else?
+
+-- TODO: we probably need to see what retail does, probably event nesting
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {})
+end
+
+function onReturn(scene, results, player)
+    player:finish_event(EVENT_ID, 0)
+end
