5ba2971c18b37b89fae08dfdef49916eefa82d4f
Joshua Goins <josh@redstrate.com>
2025-10-15 17:14:40 -0400

    Add event script for The Smith NPCs

---
+++ resources/scripts/events/custom/003/CmnDefBeginnerGuide_00327.lua
@@ -0,0 +1,9 @@
+-- Scene 0: Show the menu, the rest is handled in client-side Lua
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {0})
+end
+
+function onReturn(scene, results, player)
+    player:finish_event(EVENT_ID, 0)
+end
