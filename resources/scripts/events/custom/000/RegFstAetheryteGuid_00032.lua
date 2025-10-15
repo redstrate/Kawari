3338a05888e7522b94a8b3a656d21cf4d1bfc5d8
Joshua Goins <josh@redstrate.com>
2025-10-15 17:41:43 -0400

    Add event script for generic Aetheryte NPCs

---
+++ resources/scripts/events/custom/000/RegFstAetheryteGuid_00032.lua
@@ -0,0 +1,14 @@
+-- scene 0: greeting
+-- scene 1: menu asking about aetherytes
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {})
+end
+
+function onReturn(scene, results, player)
+    if scene == 0 then
+        player:play_scene(player.id, EVENT_ID, 00001, HIDE_HOTBAR, {})
+    else
+        player:finish_event(EVENT_ID, 0)
+    end
+end
