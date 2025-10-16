3718eae0f313dd7ea347aaddde291585f7b9ffb1
Joshua Goins <josh@redstrate.com>
2025-10-15 20:53:03 -0400

    Add simple event script for weather NPC

---
+++ resources/scripts/events/custom/002/CmnDefWeatherForeCast_00204.lua
@@ -0,0 +1,7 @@
+function onTalk(target, player, game_data)
+    player:play_scene(target, EVENT_ID, 00000, HIDE_HOTBAR, {})
+end
+
+function onReturn(scene, results, player)
+    player:finish_event(EVENT_ID, 0)
+end
