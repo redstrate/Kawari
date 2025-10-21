53ea15b66235adb6a53e2098765fb1731feebe43
Joshua Goins <josh@redstrate.com>
2025-10-21 18:55:57 -0400

    Handle nesting gil shops and inclusion shops in PreHandler

--- resources/scripts/events/generic/PreHandler.lua
+++ resources/scripts/events/generic/PreHandler.lua
@@ -3,10 +3,17 @@
 function onTalk(target, player)
     local target_event_id = GAME_DATA:get_pre_handler_target(EVENT_ID)
 
-    player:start_event(target, target_event_id, EVENT_TYPE_NEST, 5)
+    player:start_event(target, target_event_id, EVENT_TYPE_NEST, 0)
 
-    -- NOTE: not sure if we always want to call into scene 1
-    player:play_scene(target, target_event_id, 1, HIDE_HOTBAR | NO_DEFAULT_CAMERA, {})
+    -- NOTE: not sure if this is 100% true
+    local event_type = target_event_id >> 16
+    if event_type == HANDLER_TYPE_GIL_SHOP then
+        player:play_scene(target, target_event_id, 0, HIDE_HOTBAR | NO_DEFAULT_CAMERA, {})
+    elseif event_type == HANDLER_TYPE_INCLUSION_SHOP then
+        player:play_scene(target, target_event_id, 1, HIDE_HOTBAR | NO_DEFAULT_CAMERA, {})
+    else
+        print("Unknown PreHandler target event type: "..event_type)
+    end
 end
 
 function onYield(scene, results, player)
