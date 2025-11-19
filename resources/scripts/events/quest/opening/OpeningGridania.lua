7b33a1d6e609ace5abd2672a4d0649d3c711a0c3
Joshua Goins <josh@redstrate.com>
2025-11-19 18:59:05 -0500

    Use correct pop range for Gridania

--- resources/scripts/events/quest/opening/OpeningGridania.lua
+++ resources/scripts/events/quest/opening/OpeningGridania.lua
@@ -1,20 +1,22 @@
 -- TODO: when we get a new EXDSchema release, we can automatically pull in the client-side lua variables!
 -- this could be useful for the trigger IDs, for example.
 
---- Scene 0 is the actual cutscene
---- Scene 1 is the starting text asking you which controls you want to use.
---- Scene 20 is the "where are you going?!" message
---- Scene 30 is for removing the initial starting borders
+-- Scene 0 is the actual cutscene
+-- Scene 1 is the starting text asking you which controls you want to use.
+-- Scene 20 is the "where are you going?!" message
+-- Scene 30 is for removing the initial starting borders
 
---- We have to hardcode the pop range, because SqEx no longer provides this is in the Opening sheet
-POS_START = 1317553
+-- We have to hardcode the pop range, because SqEx no longer provides this is in the Opening sheet.
+-- For future reference, this is located in the QST_OP_ENPC_001 layer.
+POS_START = 2213211
 
 function onEnterTerritory(player)
-    --- Move the player into the starting position
+    -- Move the player into the starting position
     player:move_to_pop_range(POS_START)
 end
 
 function onYield(scene, results, player)
+    -- Move into the controls text after initial cutscene
     if scene == 0 then
         player:play_scene(player.id, EVENT_ID, 1, NO_DEFAULT_CAMERA | HIDE_HOTBAR, {})
         return
@@ -24,5 +26,6 @@ function onYield(scene, results, player)
 end
 
 function onEnterTrigger(player)
+    -- Play the "where are you going?!" text when entering any trigger
     player:play_scene(player.id, EVENT_ID, 20, NO_DEFAULT_CAMERA | HIDE_HOTBAR, {})
 end
