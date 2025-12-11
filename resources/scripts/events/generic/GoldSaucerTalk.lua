4f36f64af6bdf6c4cbbb9d8030518202eaa5a229
Joshua Goins <josh@redstrate.com>
2025-12-10 20:10:41 -0500

    Add support for GoldSaucerTalk NPCs
    There's at least two standalone ones in Wonder Square.

--- resources/scripts/events/generic/GoldSaucerTalk.lua
+++ resources/scripts/events/generic/GoldSaucerTalk.lua
@@ -1,6 +1,19 @@
+-- Maps some kind of IDs to GoldSaucerTalk IDs
+-- TODO: Can this be extracted from the game data somehow?
+local gold_saucer_npcs = {
+    [2686978] = 161, -- Reymanaud
+    [2686980] = 160, -- Wynkyn
+}
+
 function onTalk(target, player)
-    -- TODO: Find the correct scene number
-    --player:play_scene(target, EVENT_ID, 0, HIDE_HOTBAR, {0})
+    local scene_id = gold_saucer_npcs[EVENT_ID]
+    if scene_id == null then
+        player:send_message("Unknown Gold Saucer NPC: "..EVENT_ID)
+        player:finish_event(EVENT_ID)
+        return
+    end
+
+    player:play_scene(target, EVENT_ID, 0, HIDE_HOTBAR, {scene_id})
 end
 
 function onYield(scene, results, player)
