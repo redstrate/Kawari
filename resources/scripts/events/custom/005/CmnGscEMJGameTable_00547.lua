84f035d8517e513d96e6bba83fa01dea4e680e15
Joshua Goins <josh@redstrate.com>
2025-12-10 21:09:00 -0500

    Script the solo Mahjong tables
    This allows playing solo Mahjong games, since those
    are completely client-side.

---
+++ resources/scripts/events/custom/005/CmnGscEMJGameTable_00547.lua
@@ -0,0 +1,11 @@
+-- Mahjong Tables in Gold Saucer
+
+-- Scene 0: Start Mahjong Solo
+
+function onTalk(target, player)
+    player:play_scene(target, EVENT_ID, 0, HIDE_HOTBAR, {0})
+end
+
+function onYield(scene, results, player)
+    player:finish_event(EVENT_ID)
+end
