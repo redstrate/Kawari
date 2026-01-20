e3e9ccca8348aa1daa55bf216ef4877753361183
Joshua Goins <josh@redstrate.com>
2026-01-19 21:48:54 -0500

    Begin scripting Close to Home for Limsa Lominsa
    This makes it possible to exit the opening area for this opening now!

---
+++ resources/scripts/events/quest/001/ManSea003_00109.lua
@@ -0,0 +1,38 @@
+-- Quest: Close to Home (Limsa Lominsa)
+-- NOTE: These quests are so similar, ensure changes are synced between all of them!
+
+-- scene 0: show quest prompt
+
+local originating_npc
+
+function onTalk(target, player)
+    originating_npc = target
+
+    -- Show the quest prompt
+    player:play_scene(target, EVENT_ID, 0, HIDE_HOTBAR, {})
+end
+
+function onYield(scene, results, player)
+    if scene == 50 then
+        -- Accept the quest, this also matches up with the client-side UI
+        player:accept_quest(EVENT_ID)
+
+        local old_position = player.position
+        local old_rotation = player.rotation
+
+        -- Just like in retail, "seamlessly" transition them to the real zone:
+        player:change_territory(128, { x = old_position.x, y = old_position.y, z = old_position.z }, old_rotation)
+    end
+
+    player:finish_event(EVENT_ID)
+end
+
+function onReturn(scene, results, player)
+    if scene == 0 and results[1] == 1 then
+        -- Play the introductory text if accepted (this has to be played from Momodi)
+        player:play_scene(originating_npc, EVENT_ID, 50, HIDE_HOTBAR, {})
+        return
+    end
+
+    player:finish_event(EVENT_ID)
+end
