2df7d08de6a2bccf1dbad7df6eb58fc76d7ba23b
Joshua Goins <josh@redstrate.com>
2026-03-09 20:39:34 -0400

    Implement Standard Step and Curing Waltz

---
+++ resources/scripts/actions/159/StandardStep_15997.lua
@@ -0,0 +1,8 @@
+EFFECT_STANDARD_STEP = 1818
+
+function doAction(player)
+    effects = EffectsBuilder()
+    effects:gain_effect(EFFECT_STANDARD_STEP, 0, 15.0)
+
+    return effects
+end
---
+++ resources/scripts/actions/160/CuringWaltz_16015.lua
@@ -0,0 +1,8 @@
+POTENCY = 300
+
+function doAction(player)
+    effects = EffectsBuilder()
+    effects:heal(player.parameters:calc_heal_amount(POTENCY))
+
+    return effects
+end
