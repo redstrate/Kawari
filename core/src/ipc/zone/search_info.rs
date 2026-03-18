724a4240d50af36743842283bffba452fb755dbe
The Dax <thedax@users.noreply.github.com>
2026-03-18 16:48:11 -0400

    Implement classjob searching

--- core/src/ipc/zone/search_info.rs
+++ core/src/ipc/zone/search_info.rs
@@ -1,9 +1,11 @@
 use crate::{
-    common::{read_string, write_string},
+    common::{read_string, value_to_flag_byte_index_value, write_string},
     ipc::zone::{GrandCompany, OnlineStatusMask, SocialListUILanguages},
 };
 use binrw::binrw;
 use bitflags::bitflags;
+use strum::IntoEnumIterator;
+use strum_macros::{EnumIter, FromRepr};
 
 #[binrw]
 #[brw(little)]
@@ -65,5 +67,141 @@ impl From<&GrandCompany> for SearchUIGrandCompanies {
         }
 
         new_info
+    }
+}
+
+#[binrw]
+#[brw(repr = u8)]
+#[derive(Clone, Copy, Debug, Default, EnumIter, Eq, FromRepr, Hash, PartialEq)]
+pub enum SearchUIClassJob {
+    #[default]
+    Gladiator = 0,
+    Pugilist = 1,
+    Marauder = 2,
+    Lancer = 3,
+    Archer = 4,
+    Conjurer = 5,
+    Thaumaturge = 6,
+    Carpenter = 7,
+    Blacksmith = 8,
+    Armorer = 9,
+    Goldsmith = 10,
+    Leatherworker = 11,
+    Weaver = 12,
+    Alchemist = 13,
+    Culinarian = 14,
+    Miner = 15,
+    Botanist = 16,
+    Fisher = 17,
+    Paladin = 18,
+    Monk = 19,
+    Warrior = 20,
+    Dragoon = 21,
+    Bard = 22,
+    WhiteMage = 23,
+    BlackMage = 24,
+    Arcanist = 25,
+    Summoner = 26,
+    Scholar = 27,
+    Rogue = 28,
+    Ninja = 29,
+    Machinist = 30,
+    DarkKnight = 31,
+    Astrologian = 32,
+    Samurai = 33,
+    RedMage = 34,
+    BlueMage = 35,
+    Gunbreaker = 36,
+    Dancer = 37,
+    Reaper = 38,
+    Sage = 39,
+    Viper = 40,
+    Pictomancer = 41,
+}
+
+#[binrw]
+#[brw(little)]
+#[derive(Clone, Copy, Default, Hash, PartialEq)]
+pub struct SearchUIClassJobMask {
+    pub flags: [u8; 8],
+}
+
+impl From<[u8; 8]> for SearchUIClassJobMask {
+    fn from(flags: [u8; 8]) -> Self {
+        Self { flags }
+    }
+}
+
+impl SearchUIClassJobMask {
+    pub fn from_searchui_classjob(classjob: SearchUIClassJob) -> Self {
+        let mut classjobs = Self::default();
+        classjobs.set_classjob(classjob);
+
+        classjobs
+    }
+
+    pub fn mask(&self) -> Vec<SearchUIClassJob> {
+        let mut classjobs = Vec::new();
+
+        for classjob in SearchUIClassJob::iter() {
+            let (value, index) = value_to_flag_byte_index_value(classjob as u32);
+            if self.flags[index as usize] & value == value {
+                classjobs.push(classjob);
+            }
+        }
+
+        classjobs
+    }
+
+    pub fn set_classjob(&mut self, classjob: SearchUIClassJob) {
+        let (value, index) = value_to_flag_byte_index_value(classjob as u32);
+        self.flags[index as usize] |= value;
+    }
+
+    pub fn remove_classjob(&mut self, classjob: SearchUIClassJob) {
+        let (value, index) = value_to_flag_byte_index_value(classjob as u32);
+        self.flags[index as usize] ^= value;
+    }
+
+    pub fn has_classjob(&self, classjob: SearchUIClassJob) -> bool {
+        let (value, index) = value_to_flag_byte_index_value(classjob as u32);
+        self.flags[index as usize] & value == value
+    }
+}
+
+impl std::fmt::Debug for SearchUIClassJobMask {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.flags.iter().all(|x| *x == 0) {
+            return write!(f, "None");
+        }
+
+        if self.mask().is_empty() {
+            write!(f, "{:#?}", self.flags)
+        } else {
+            write!(f, "{:?}", self.mask())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_searchui_classjob_lancer() {
+        let mask: [u8; 8] = [8, 0, 0, 0, 0, 0, 0, 0];
+        assert_eq!(
+            SearchUIClassJobMask::from(mask).mask(),
+            vec![SearchUIClassJob::Lancer]
+        );
+    }
+
+    #[test]
+    fn read_searchui_classjob_archer() {
+        let mask: [u8; 8] = [16, 0, 0, 0, 0, 0, 0, 0];
+        assert_eq!(
+            SearchUIClassJobMask::from(mask).mask(),
+            vec![SearchUIClassJob::Archer]
+        );
     }
 }
