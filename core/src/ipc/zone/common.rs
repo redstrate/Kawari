e53d15c5724970084cbff2ea53434c28c5274810
Mana <rhithin-mana@users.noreply.github.com>
2026-04-08 23:06:14 +0200

    Add housing related types

--- core/src/ipc/zone/common.rs
+++ core/src/ipc/zone/common.rs
@@ -1,4 +1,5 @@
 use binrw::binrw;
+use bitflags::bitflags;
 
 #[binrw]
 #[brw(little)]
@@ -51,4 +52,80 @@ pub enum HousingAppealTag {
     Bakery = 22,
     UnderRenovation = 23,
     ConcertHall = 24,
+}
+
+/// Represents the size of a housing plot.
+#[binrw]
+#[brw(repr = u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum PlotSize {
+    #[default]
+    Small = 0,
+    Medium = 1,
+    Large = 2,
+}
+
+/// Represents the purchase system used for a plot.
+/// FCFS hasn't been used since the addition of Empyreum in 6.0.
+#[binrw]
+#[brw(repr = u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum PurchaseType {
+    #[default]
+    Unavailable = 0,
+
+    /// First come, First served
+    FCFS = 1,
+
+    Lottery = 2,
+}
+
+/// Represents the allowed type of tenant for a specific plot or entire ward.
+#[binrw]
+#[brw(repr = u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum TenantType {
+    #[default]
+    Any = 0,
+
+    FreeCompany = 1,
+
+    Personal = 2,
+}
+
+/// Represents the availability of a specific plot during the *current* lottery cycle/
+#[binrw]
+#[brw(repr = u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum AvailabilityType {
+    /// Plot does not participate in current lottery cycle.
+    #[default]
+    Unavailable = 0,
+
+    /// Plot is available for bidding.
+    Available = 1,
+
+    /// Plot participated in lottery cycle, no more bidding allowed.
+    InResultsPeriod = 2,
+}
+
+#[binrw]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct HousingFlags(u8);
+
+impl Default for HousingFlags {
+    fn default() -> Self {
+        HousingFlags::NONE
+    }
+}
+
+bitflags! {
+    impl HousingFlags: u8 {
+        const NONE = 0;
+        const PLOT_OWNED = 1 << 0;
+        const VISITORS_ALLOWED = 1 << 1;
+        const HAS_SEARCH_COMMENT = 1 << 2;
+        const HOUSE_BUILT = 1 << 3;
+        const OWNED_BY_FC = 1 << 4;
+    }
 }
