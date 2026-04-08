d075eedcdfbd0567ee546d05999596a58fc8f3e8
Mana <rhithin-mana@users.noreply.github.com>
2026-04-08 23:26:28 +0200

    Refactor `HousingWardInfo` packet
    - Extract into `HousingWardInfo` struct
    - Add missing fields

--- core/src/ipc/zone/server/housing_ward.rs
+++ core/src/ipc/zone/server/housing_ward.rs
@@ -1,13 +1,33 @@
 use binrw::binrw;
 
-use crate::common::{read_string, write_string};
+use crate::{
+    common::{LandId, read_string, write_string},
+    ipc::zone::{HousingAppealTag, HousingFlags, PurchaseType, TenantType},
+};
 
 #[binrw]
 #[derive(Debug, Clone, Default)]
-pub struct HousingWardMenuSummaryItem {
+pub struct HousingWardInfo {
+    pub land_id: LandId,
+
+    #[br(count = 60)]
+    #[bw(pad_size_to = 60 * HousingWardSummaryItem::SIZE)]
+    pub house_summaries: Vec<HousingWardSummaryItem>,
+
+    pub purchase_type: PurchaseType,
+    pub unk1: u8,
+    pub tenant_type: TenantType,
+
+    #[brw(pad_after = 1)]
+    terminator: u32,
+}
+
+#[binrw]
+#[derive(Debug, Clone, Default)]
+pub struct HousingWardSummaryItem {
     pub plot_price: u32,
-    /// Flags? Unknown what they mean, needs research
-    pub flags: u32,
+    pub flags: HousingFlags,
+    pub tags: [HousingAppealTag; 3],
     #[brw(pad_size_to = 32)]
     #[br(count = 32)]
     #[br(map = read_string)]
@@ -15,7 +35,7 @@ pub struct HousingWardMenuSummaryItem {
     pub name: String,
 }
 
-impl HousingWardMenuSummaryItem {
+impl HousingWardSummaryItem {
     pub const SIZE: usize = 40;
 }
 
@@ -27,6 +47,6 @@ mod tests {
 
     #[test]
     fn housing_ward_size() {
-        ensure_size::<HousingWardMenuSummaryItem, { HousingWardMenuSummaryItem::SIZE }>();
+        ensure_size::<HousingWardSummaryItem, { HousingWardSummaryItem::SIZE }>();
     }
 }
