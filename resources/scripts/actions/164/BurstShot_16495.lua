dbbb73288720d95fbac067b3a44156764eebad39
Joshua Goins <josh@redstrate.com>
2026-04-14 14:15:09 -0400

    Add Burst Shot action

---
+++ resources/scripts/actions/164/BurstShot_16495.lua
@@ -0,0 +1,8 @@
+POTENCY = 220
+
+function doAction(player)
+    effects = EffectsBuilder()
+    effects:damage(DAMAGE_KIND_NORMAL, DAMAGE_TYPE_SLASHING, DAMAGE_ELEMENT_UNASPECTED, player.parameters:calc_physical_damage(POTENCY))
+
+    return effects
+end
