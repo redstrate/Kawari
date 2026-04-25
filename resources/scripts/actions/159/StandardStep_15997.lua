5246b0a099bfd194bfb6f772d372d274795dd78b
Joshua Goins <josh@redstrate.com>
2026-04-25 11:12:11 -0400

    Mark standard step status effect as coming from yourself

--- resources/scripts/actions/159/StandardStep_15997.lua
+++ resources/scripts/actions/159/StandardStep_15997.lua
@@ -2,7 +2,7 @@ EFFECT_STANDARD_STEP = 1818
 
 function doAction(player, in_combo)
     effects = EffectsBuilder()
-    effects:gain_effect(EFFECT_STANDARD_STEP, 0, 15.0)
+    effects:gain_effect_self(EFFECT_STANDARD_STEP, 0, 15.0)
 
     return effects
 end
